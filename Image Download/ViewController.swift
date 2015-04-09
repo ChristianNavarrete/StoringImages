d13dd3b416828d14ee88a4d79b1992de775a5d30
HoodsDream <idinkyy@gmail.com>
2015-04-08 19:23:42 -0700

    initial commit

--- Image Download/ViewController.swift
+++ Image Download/ViewController.swift
@@ -9,17 +9,60 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
 
+    @IBOutlet weak var image: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
+        let url = NSURL(string: "http://okp-cdn.okayplayer.com/wp-content/uploads/2013/03/kendrick-lamar-lead1.jpg")
+        
+        let urlRequest = NSURLRequest(URL: url!)
+        
+        NSURLConnection.sendAsynchronousRequest(urlRequest, queue: NSOperationQueue.mainQueue(), completionHandler: {
+            response, data, error in
+            
+            if error != nil {
+                
+                println("There was an error")
+                
+            } else {
+                
+                let image = UIImage(data: data)
+                
+                var documentsDirectory:String?
+                
+                var paths:[AnyObject] = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.UserDomainMask, true)
+                
+                if paths.count > 0 {
+                    
+                    documentsDirectory = paths[0] as? String
+                    
+                    var savePath = documentsDirectory! + "/bach.jpg"
+                    
+                    NSFileManager.defaultManager().createFileAtPath(savePath, contents: data, attributes: nil)
+                    
+                    self.image.image = UIImage(named: savePath)
+                                        
+                }
+                
+            }
+            
+            
+        })
+        
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
